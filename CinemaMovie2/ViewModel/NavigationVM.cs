307163cb2238ccfcd47a31b10772350ef8b59c36
MeyTeplon <ASUS@LAPTOP-VTDSD35M>
2023-01-09 21:52:32 +0700

    cek bagian relayCommand karo NavigationVM.cs

--- CinemaMovie2/ViewModel/NavigationVM.cs
+++ CinemaMovie2/ViewModel/NavigationVM.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using CinemaMovie2.Model;
 using CinemaMovie2.Utilities;
+using CinemaMovie2.View;
 
 namespace CinemaMovie2.ViewModel
 {
@@ -23,6 +24,7 @@ namespace CinemaMovie2.ViewModel
         public ICommand PaymentCommand { get; set; }
         public ICommand TvShowsCommand { get; set; }
         public ICommand SettingCommand { get; set; }
+        public ICommand HistoryCommand { get; set; }
 
         private void Home(object obj) => CurrentView = new HomeVM();
         private void Movies(object obj) => CurrentView = new MoviesVM();
@@ -30,6 +32,7 @@ namespace CinemaMovie2.ViewModel
         private void Payment(object obj) => CurrentView = new PaymentVM();
         private void TvShows(object obj) => CurrentView = new TvShowVM();
         private void Setting(object obj) => CurrentView = new SettingVM();
+        private void History(object obj) => CurrentView = new HistoryVM();
 
         public NavigationVM()
         {
@@ -39,6 +42,7 @@ namespace CinemaMovie2.ViewModel
             PaymentCommand = new RelayCommand(Payment);
             TvShowsCommand = new RelayCommand(TvShows);
             SettingCommand = new RelayCommand(Setting);
+            HistoryCommand = new RelayCommand(History);
 
             //start page
             CurrentView = new HomeVM();
